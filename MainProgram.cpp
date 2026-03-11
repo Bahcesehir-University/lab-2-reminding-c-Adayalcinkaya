3186119fafaa844d7a6b345d2c12ab0c6440b2bb
Adayalcinkaya <ada.yalcinkaya@bahcesehir.edu.tr>
2026-03-11 16:37:51 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -14,6 +14,7 @@ int addNumbers(int a, int b)
 {
     // TODO
     // Return the sum of a and b
+    return a + b;
 
 }
 
@@ -30,6 +31,11 @@ int sumUpToN(int n)
     // TODO
     // Using a loop calculate the sum
     // 1 + 2 + 3 + ... + n
+    int sum = 0;
+    for(int i = 0 ; i < n ; i++){
+        sum += i;
+    }
+    return sum;
 }
 
 
@@ -44,6 +50,13 @@ int findMaximum(vector<int> numbers)
 {
     // TODO
     // Return the largest number in the vector
+    int max = numbers[0];
+    for(int i = 1 ; i < numbers.size() ; i++){
+        if(numbers[i] = max){
+            max = numbers[i];
+        }
+    }
+    return max;
 }
 
 
@@ -58,6 +71,11 @@ string reverseString(string text)
 {
     // TODO
     // Return the reversed version of the string
+    string reversed = "";
+    for(int i = text.length() - 1 ; i >= 0 ; i--){
+        reversed += text[i];
+    }
+    return reversed;
 }
 
 
@@ -79,6 +97,14 @@ public:
     // TODO
     // Create a constructor that receives
     // name and grade
+    Student(string n,int g){
+        name = n;
+        grade = g;
+    }
+    void printInfo(){
+        cout<<"Name : "<<name<<endl;
+        cout<<"Grade : "<<grade<<endl;
+    }
 
 
     // TODO
